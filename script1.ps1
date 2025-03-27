17c476cba98fbc27a7f424c04338e26249e7532b
unknown <rei42782@gmail.com>
2025-03-26 17:55:23 -0700

    yo

---
+++ script1.ps1
@@ -0,0 +1,15 @@
+﻿#DESEC SECURITY - PING SWEEP
+param($pi)
+$pi = Read-host "Digite a rede"
+if (!$pi){
+    echo "Desec Security" 
+    echo "Exemplo de uso: .\ script.ps1 192.168.0"
+} else {
+
+foreach ($ip in 1..254){
+#para usar o split(similar ao cut no bash) devemos guardar a resposta em uma variavel
+try {$resp = ping -n 1 "$pi.$ip" | Select-String "bytes=32"
+$resp.Line.split(' ')[2] -replace ":",""
+} catch{}
+}
+}
